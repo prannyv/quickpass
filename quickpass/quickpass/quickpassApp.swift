35848371b08ab13553a0d4f81c8f210fd7b914f3
Etn12345 <156725331+Etn12345@users.noreply.github.com>
2026-01-17 01:17:25 -0500

    created initial app

---
+++ quickpass/quickpass/quickpassApp.swift
@@ -0,0 +1,32 @@
+//
+//  quickpassApp.swift
+//  quickpass
+//
+//  Created by Eileen Zhao on 2026-01-17.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct quickpassApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
